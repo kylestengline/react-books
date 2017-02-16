3a8060726cc62b3494b611014a1e85186ff44b9d
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-15 16:53:37 -0800

    adds book list and reducer for books

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import BooksReducer from './reducer_boos';
 
+// This is how we 'wire' the reducer into our application
+// combineReducers is a built-in function
+// we pass in the booksreducer as to combine the reducers.
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  books: BooksReducer
 });
 
 export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+// We write export default so we can access this function in other files to 'wire' it into our application.
+export defualt function() {
+  return [
+    {title: 'Javascript: The Good Parts' },
+    {title: 'Harry Potter' },
+    {title: 'The Dark Tower' },
+    {title: 'Eloquent Ruby' }
+  ]
+}
