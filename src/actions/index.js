461bb00838a85fdd4411a4584708f764d99f447b
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-18 18:16:20 -0800

    adds first action creator with type and payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,11 @@
 //Action creator: just a function
 export function selectBook(book) {
-  console.log('A book has been selected:', book.title);
+  // SelectBook is an action creator and needs to return and action.
+  // An object with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
+  //Actions always contains a type and sometimes contains a payload.
+  //Type is always uppercase, usually a string, always separated by keywords
 }
