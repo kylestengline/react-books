36c1c7e47825c5e8bf07db98ff0fb6fecf0e4d24
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-18 17:29:22 -0800

    comments on code

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,4 +1,6 @@
 // We write export default so we can access this function in other files to 'wire' it into our application.
+// The issue with the below is that it makes our applcation static. 
+// The state of our app will always be this list of books.
 export default function() {
   return [
     {title: 'Javascript: The Good Parts' },
