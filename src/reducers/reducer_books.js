7c9df034c8965bd0b868050a525f976570ac6e38
Kyle Stengline <kyle.stengline@gmail.com>
2017-02-18 19:08:58 -0800

    adds more details for books, updates view to show more details for selected book

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -3,9 +3,9 @@
 // The state of our app will always be this list of books.
 export default function() {
   return [
-    {title: 'Javascript: The Good Parts' },
-    {title: 'Harry Potter' },
-    {title: 'The Dark Tower' },
-    {title: 'Eloquent Ruby' }
+    {title: 'Javascript: The Good Parts', pages: 101 },
+    {title: 'Harry Potter', pages: 100 },
+    {title: 'The Dark Tower', pages: 20 },
+    {title: 'Eloquent Ruby', pages: 5 }
   ];
 }
